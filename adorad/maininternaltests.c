65d7312f61a697a5c691748af9d31f156ed532ad
Jason Dsouza <jasmcaus@gmail.com>
2021-08-06 10:50:26 +0530

    Update maininternaltests

--- adorad/maininternaltests.c
+++ adorad/maininternaltests.c
@@ -16,14 +16,14 @@ int main(int argc, const char* const argv[]) {
     printf("Lexing finished...\n");
     double total = duration(st, end);
 
-    // printf("\033[1;32m\nTokens Vector: \033[0m\n");
-    // for(Token* tok = vec_begin(lexer->toklist); tok <= vec_end(lexer->toklist); tok++) {
-    //     printf("TOKEN(%s, \"%s\")\n", token_to_buff(tok->kind)->data, tok->value->data);
-    // }
-    // printf("\nTotal time = %lfs\n", total);
+    printf("\033[1;32m\nTokens Vector: \033[0m\n");
+    for(Token* tok = vec_begin(lexer->toklist); tok <= vec_end(lexer->toklist); tok++) {
+        printf("TOKEN(%s, \"%s\")\n", token_to_buff(tok->kind)->data, tok->value->data);
+    }
+    printf("\nTotal time = %lfs\n", total);
 
-    // printf("Number of tokens = %d\n", vec_size(lexer->toklist));
-    // printf("Total allocated memory (in bytes) = %d\n", lexer->toklist->internal.objsize * vec_size(lexer->toklist));
+    printf("Number of tokens = %d\n", vec_size(lexer->toklist));
+    printf("Total allocated memory (in bytes) = %d\n", lexer->toklist->internal.objsize * vec_size(lexer->toklist));
     
     lexer_free(lexer);
     return 0; 
